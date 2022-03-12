6028a94c28b5b74baa10861b36739eab6c0a2885
Carlos <carlos.piccolini@hotmail.com>
2022-03-12 16:52:29 -0300

    Minotaur 3D model changed
    The 3d model and animations of the Minotaur were updated.

---
+++ Assets/Scripts/Minotaur/MinotaurControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* MinotaurControl Class
+** Maneja y entrelaza a todos los scripts Minotaur y es la única clase donde se encuentra el Update()
+*/
+public class MinotaurControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
