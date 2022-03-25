5a8ac6f409346ddb55b8c3f883035decd95b303c
Carlos <carlos.piccolini@hotmail.com>
2022-03-25 16:29:37 -0300

    Modifications to stamina bar
    Thee is  a bug in the stamina bar that it does not get de stamina max value by method, it has to be done by a literal. Later I will search on this bug. (It is a problem because it is only in the build, not in the editor mode).

--- Assets/Scripts/Hud/StaminaBar.cs
+++ Assets/Scripts/Hud/StaminaBar.cs
@@ -1,19 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;    // Necesario para usar TextMeshProUGUI
+
 
 public class StaminaBar : MonoBehaviour
 {
-    public Slider staminaBar;   // Referencia a la barra de stamina en el HUD
-    public GameObject player;   // Referencia al player cuya stamina es representada en la barra
-    PlayerStamina playerStamina;// Script de la stamina dentro del player
+    public Slider staminaBar;           // Referencia a la barra de stamina en el HUD
+    public GameObject player;           // Referencia al player cuya stamina es representada en la barra
+    public PlayerStamina playerStamina; // Script de la stamina dentro del player
 
     // Start is called before the first frame update
     void Start()
     {
         playerStamina = player.GetComponent<PlayerStamina>();
-        staminaBar.maxValue = playerStamina.GetMaxStamina();
+        // BUG: POR MOTIVO DESCONOCIDO ESTA FUNCIÓN DEVUELVE O Y NO 100 COMO DEBERIA (ESTO PASA SOLO EN LA BUILD)
+        // staminaBar.maxValue = playerStamina.GetMaxStamina();
+        staminaBar.maxValue = 100f;
         staminaBar.value = playerStamina.GetStamina();
     }
 
